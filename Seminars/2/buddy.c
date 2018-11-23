859ec642c8952611d78d60cec7be045172bf88f0
felix <toppar@kth.se>
2018-11-23 13:14:33 +0100

    fixed new block creation

--- Seminars/2/buddy.c
+++ Seminars/2/buddy.c
@@ -1,1 +1,35 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <assert.h>
+#define MIN 32
+#define LEVELS 8
+#define PAGE 4096
 
+enum flag {Free, Taken};
+
+struct head{
+  enum flag status;
+  short int level;
+  struct head *next;
+  struct head *prev;
+};
+
+struct head *new(){
+  //here we allocate a full 4096 byte
+  struct head *new = (struct head *) mmap(NULL, PAGE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if(new == MAP_FAILED){
+    return NULL;
+  }
+  assert(((long int) new & 0xfff) == 0); //last 12 bits should be zero
+  new->status = Free;
+  new->level = LEVELS - 1;
+  return new;
+};
+
+void test(){
+  printf("new block created with level %d\n", new()->level );
+}
+
+int main(){
+  test();
+}
