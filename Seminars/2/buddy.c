f58cd6e9de5845c7d1678e1dee723e585d1c525d
felix <toppar@kth.se>
2018-11-24 12:59:14 +0100

    added some comments

--- Seminars/2/buddy.c
+++ Seminars/2/buddy.c
@@ -8,29 +8,35 @@
 
 enum flag {Free, Taken};
 
+//This is the header of each block
 struct head{
   enum flag status;
   short int level;
   struct head *next;
   struct head *prev;
 };
-struct head *new(){
-  //here we allocate a full 4096 byte
-  struct head *new = (struct head *) mmap(NULL, PAGE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+//This function creates a new block
+struct head* new(){
+  struct head *new = (struct head*) mmap(NULL, PAGE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if(new == MAP_FAILED){
     return NULL;
   }
-  assert(((long int) new & 0xfff) == 0); //last 12 bits should be zero
+  assert(((long int) new & 0xfff) == 0);
   new->status = Free;
   new->level = LEVELS - 1;
   return new;
 };
-struct head *buddy(struct head* block){
+
+//This function find the buddy of the entered block 
+struct head* buddy(struct head* block){
   int index = block->level;
   long int mask = 0x1 << (index + MIN);
   return (struct head*)((long int ) block ^ mask);
 }
-struct head *merge(struct head* block, struct head* sibling){
+
+//This function merge two blocks into one 
+struct head* merge(struct head* block, struct head* sibling){
   struct head *primary;
   if(sibling < block){
     primary = sibling;
@@ -40,16 +46,22 @@ struct head *merge(struct head* block, struct head* sibling){
   primary->level = primary->level + 1;
   return primary;
 }
+
+//This function hides the header of the entered block
 void *hide(struct head* block){
   return (void*)(block + 1);
 }
+
+//This function finds the full block with its header
 struct head *magic(void* memory){
   return ((struct head*) memory - 1);
 }
+
+//This function find the level of a block given the entered size
 int level(int size){
   int req = size + sizeof(struct  head);
   int i = 0;
-  req = req >> MIN;
+  req = req >> MIN; 
   while(req > 0){
     i++;
     req = req >> 1;
@@ -57,39 +69,24 @@ int level(int size){
   return i;
 }
 
-struct head *split(struct head *block, int index){
+//This function splits a block and returns the new block
+struct head* split(struct head* block, int index){
   long int mask = 0x1 << (index + MIN);
     return (struct head*)((long int) block | mask);
 }
 
+//This function is used to test the different functionallity above
 void test(){
   struct head *first = new();
   struct head *second = new();
   struct head *third = new();
   first->next = second;
-  second->prev = first;
+  second->prev = first; 
   second->next = third;
   third->prev = second;
 
   printf("1st block = %p, with (level = %d, prev = %p, next = %p)\n",first, first->level, first->prev, first->next);
   printf("2st block = %p, with (level = %d, prev = %p, next = %p)\n",second, second->level, second->prev, second->next);
   printf("3st block = %p, with (level = %d, prev = %p, next = %p)\n",third, third->level, third->prev, third->next);
-
-
-  struct head *left = split(first, 7);
-  void* p = hide(left);
-  void* r = magic(p);
-  printf("left = %p, p = %p, r = %p", left, p, r );
-  /*
-  printf("block %p splited into %p with level = %d\n",first, left, left->level);
-  struct head *leftLeft = split(left, 6);
-  struct head *lmerged = merge(left,leftLeft);
-  struct head *firstRestored = merge(first,lmerged);
-
-  printf("block = %p, with (level = %d, prev = %p, next = %p)\n",firstRestored, firstRestored->level, firstRestored->prev, firstRestored->next);
-  */
-
-
-
 
 }
