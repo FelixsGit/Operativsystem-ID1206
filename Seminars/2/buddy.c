d29fb8059b564d51847258656180e62035d2e0f6
felix <toppar@kth.se>
2018-11-25 15:05:33 +0100

    binary tree find implemented

--- Seminars/2/buddy.c
+++ Seminars/2/buddy.c
@@ -21,37 +21,6 @@ struct head{
   struct head *rightChildBlock;
 };
 
-//This function adds the entered block to the head of the linkedList at the entered level
-/*
-void push(struct head* blockToAdd, short int level){
-  if(freeLists[level] == NULL){
-    freeLists[level] = blockToAdd;
-    blockToAdd->level = level;
-    blockToAdd->prev = NULL;
-    blockToAdd->next = NULL;
-  }else{
-    blockToAdd->next = freeLists[level];
-    blockToAdd->level = level;
-    blockToAdd->prev = NULL;
-    blockToAdd->next->prev = blockToAdd;
-    freeLists[level] = blockToAdd;
-  }
-}
-
-//This function removes the first block in the linkedList at the enterd level
-void delete(short int level){
-  printf("Deleting the first block at level %d\n", level);
-  if(freeLists[level] == NULL){
-    return;
-  }else if(freeLists[level]->next != NULL){
-    freeLists[level] = freeLists[level]->next;
-    freeLists[level]->prev = NULL;
-  }else{
-    freeLists[level] = NULL;
-  }
-}
-
-*/
 //This function creates a new block
 void newMemory() {
   memory = mmap(NULL, PAGE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -221,69 +190,48 @@ void *balloc(size_t size){
   }
 }
 
-int _print_t(struct head* tree, int is_left, int offset, int depth, char s[20][255])
-{
+int _print_t(struct head* tree, int is_left, int offset, int depth, char s[20][255]){
     char b[20];
     int width = 5;
-
     if (!tree) return 0;
-
     sprintf(b, "(%03d)", tree->status);
-
     int left  = _print_t(tree->leftChildBlock,  1, offset,                depth + 1, s);
     int right = _print_t(tree->rightChildBlock, 0, offset + left + width, depth + 1, s);
-
 #ifdef COMPACT
     for (int i = 0; i < width; i++)
         s[depth][offset + left + i] = b[i];
-
     if (depth && is_left) {
-
         for (int i = 0; i < width + right; i++)
             s[depth - 1][offset + left + width/2 + i] = '-';
-
         s[depth - 1][offset + left + width/2] = '.';
-
     } else if (depth && !is_left) {
-
         for (int i = 0; i < left + width; i++)
             s[depth - 1][offset - width/2 + i] = '-';
-
         s[depth - 1][offset + left + width/2] = '.';
     }
 #else
     for (int i = 0; i < width; i++)
         s[2 * depth][offset + left + i] = b[i];
-
     if (depth && is_left) {
-
         for (int i = 0; i < width + right; i++)
             s[2 * depth - 1][offset + left + width/2 + i] = '-';
-
         s[2 * depth - 1][offset + left + width/2] = '+';
         s[2 * depth - 1][offset + left + width + right + width/2] = '+';
-
     } else if (depth && !is_left) {
-
         for (int i = 0; i < left + width; i++)
             s[2 * depth - 1][offset - width/2 + i] = '-';
-
         s[2 * depth - 1][offset + left + width/2] = '+';
         s[2 * depth - 1][offset - width/2 - 1] = '+';
     }
 #endif
-
     return left + width + right;
 }
 
-void print_t(struct head* tree)
-{
+void print_t(struct head* tree){
     char s[20][255];
     for (int i = 0; i < 20; i++)
         sprintf(s[i], "%80s", " ");
-
     _print_t(tree, 0, 0, 0, s);
-
     for (int i = 0; i < 20; i++)
         printf("%s\n", s[i]);
 }
