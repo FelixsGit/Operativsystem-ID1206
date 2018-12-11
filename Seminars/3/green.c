271238ec63c0431cdd899ab0ca7b5b04d5a1c848
felix <toppar@kth.se>
2018-12-11 14:25:10 +0100

    fixed mutex and its tests

--- Seminars/3/green.c
+++ Seminars/3/green.c
@@ -3,17 +3,22 @@
 #include <stdio.h>
 #include <assert.h>
 #include "green.h"
+#include <signal.h>
+#include <sys/time.h>
 
+#define PERIOD 100
 #define FALSE 0
 #define TRUE 1
 #define STACK_SIZE 4096
 
+static sigset_t block;
 static ucontext_t main_cntx = {0};
 static green_t main_green = {&main_cntx, NULL, NULL, NULL, NULL, FALSE};
 static green_t *running = &main_green;
 
 static void init() __attribute__((constructor));
 
+void timer_handler(int);
 void addToReadyQueue(green_t* thread);
 void tellNextToRun();
 void addToQueue(green_t** queue, green_t* threadToAdd);
@@ -21,8 +26,51 @@ green_t* popFromQueue(green_t** queue);
 
 //This function is used at the start to initialize the program
 void init(){
-	getcontext(&main_cntx);
+	//getcontext(&main_cntx);
+	sigemptyset(&block);
+	sigaddset(&block, SIGVTALRM);
+	struct sigaction act = {0};
+	struct timeval interval;
+	struct itimerval period;
+	act.sa_handler = timer_handler;
+	assert(sigaction(SIGVTALRM, &act, NULL) == 0);
+	interval.tv_sec = 0;
+	interval.tv_usec = PERIOD;
+	period.it_interval = interval;
+	period.it_value = interval;
+	setitimer(ITIMER_VIRTUAL, &period, NULL);
 }
+
+int green_mutex_init(green_mutex_t *mutex){
+	mutex->taken = FALSE;
+	mutex->susp = NULL;
+}
+
+int green_mutex_lock(green_mutex_t *mutex){
+	sigprocmask(SIG_BLOCK, &block, NULL);
+	green_t *susp = running;
+	while(mutex->taken){
+		//suspend the running thread
+		addToQueue(&(mutex->susp), susp);
+		//tell next thread to run
+		tellNextToRun();
+		swapcontext(susp->context, running->context);
+	}
+	mutex->taken = TRUE;
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
+	return 0;
+}
+
+int green_mutex_unlock(green_mutex_t *mutex){
+	sigprocmask(SIG_BLOCK, &block, NULL);
+	//move suspended threads to ready queue;
+	addToReadyQueue(mutex->susp);
+	mutex->susp = NULL;
+	mutex->taken = FALSE;
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
+	return 0; 
+}
+
 
 void addToReadyQueue(green_t *ready){
 	addToQueue(&running, ready);
@@ -53,23 +101,29 @@ green_t* popFromQueue(green_t **queue){
 
 //This function is the 'main running part of the thread'
 void green_thread(){
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 	//Thread starts its execution
 	green_t *this = running;
 	(*this->fun)(this->arg);
 	//Thread is not done with its execution
 	//Place waiting (joining) thread in the ready queue
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	if(this->join != NULL){
 		addToReadyQueue(this->join);
 	}
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 	//free alocated memory structures
 	free(this->context->uc_stack.ss_sp);
 	free(this->context);
 	//set the status to zombie
 	this->zombie = TRUE;
 	//find the next thread to run
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	tellNextToRun();
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 	//thread now dead
 	setcontext(running->context);
+
 }
 
 //This function is used to create a new green thread 
@@ -86,19 +140,23 @@ int green_create(green_t *new, void*(*fun)(void*), void *arg){
 	new->next = NULL;
 	new->join = NULL;
 	new->zombie = FALSE;
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	addToReadyQueue(new);
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 	return 0;
 }
 
 //This function tells the running thread to wait and then tells the next 
 //thread in the free list to running and makes the context switch.
 int green_yield(){
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	green_t * susp = running;
 	//add susp to ready queue
 	addToReadyQueue(susp);
 	//select the next thread for execution
 	tellNextToRun();
 	swapcontext(susp->context, running->context);
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 	return 0;
 }
 
@@ -107,6 +165,7 @@ int green_join(green_t *thread){
 		return 0;
 	}
 	green_t *susp = running;
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	//Add to waiting thread
 	if(thread->join == NULL){
 		thread->join = susp;
@@ -120,25 +179,35 @@ int green_join(green_t *thread){
 	//Select the next thread for execution
 	tellNextToRun();
 	swapcontext(susp->context, running->context);
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 	return 0;
 }
 
+void timer_handler(int sig){
+	//printf("timmer triggered\n");
+	green_yield();
+}
 
 void green_cond_init(struct green_cond_t *cond){
 	cond->suspendedThreads = NULL;
 }
 
 void green_cond_wait(struct green_cond_t *cond){
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	addToQueue(&(cond->suspendedThreads), running);
 	green_t *susp = running;
 	tellNextToRun();
 	swapcontext(susp->context, running->context);
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 }
 
 void green_cond_signal(struct green_cond_t *cond){
+	sigprocmask(SIG_BLOCK, &block, NULL);
 	green_t *threadToSignal = popFromQueue(&cond->suspendedThreads);
 	addToReadyQueue(threadToSignal);
+	sigprocmask(SIG_UNBLOCK, &block, NULL);
 }
+
 
 
 
--- Seminars/3/test.c
+++ Seminars/3/test.c
@@ -2,50 +2,84 @@
 #include "green.h"
 
 
-//test 2
+int counter = 0;
 int flag = 0;
 green_cond_t cond;
-void *test2(void *arg){
+green_mutex_t mutex;
+
+void *testTimer(void *arg){
 	int id = *(int*)arg;
-	int loop = 4;
+	int loop = 40000;
 	while(loop > 0){
 		if(flag == id){
+			printf("thread %d: %d\n", id, loop);
 			loop--;
-			flag = (id + 1) % 2;
+			flag = (id + 1) % 4;
 			green_cond_signal(&cond);
 		}else{
+			green_cond_signal(&cond);
 			green_cond_wait(&cond);
 		}
 	}
 }
 
-/*
-//test 1
-void *test(void *arg){
+/**
+The program falls short without the signal before the wait.
+With the signal on the condition before the wait the program works 
+as intended.
+**/
+
+void *testStd(void *arg){
 	int i = *(int*)arg;
-	int loop = 4;
+	int loop = 40000;
 	while(loop > 0){
 		printf("thread %d: %d\n", i, loop);
 		loop--;
 		green_yield();
 	}
 }
-*/
+
+
+void* testSharedResource(void* arg) {
+  int id = *(int*)arg;
+  for(int i = 0; i < 100000; i++) {
+    //green_mutex_lock(&mutex);
+    int timeWaste = 0;
+    while(timeWaste < 1000){
+    	timeWaste++;
+    }
+    counter++;
+    //green_mutex_unlock(&mutex);
+  }
+}
+
 int main(){
 	green_t g0;
 	green_t g1;
+	green_t g2;
+	green_t g3;
 
 	int a0 = 0;
 	int a1 = 1;
+	int a2 = 2;
+	int a3 = 3;
 
+	green_mutex_init(&mutex);
 	green_cond_init(&cond);
-	green_create(&g0, test2, &a0);
-	green_create(&g1, test2, &a1);
+	green_create(&g0, testSharedResource, &a0);
+	green_create(&g1, testSharedResource, &a1);
+	green_create(&g2, testSharedResource, &a2);
+	green_create(&g3, testSharedResource, &a3);
 
 	green_join(&g0);
 	printf("Thread nr %d done\n", a0);
 	green_join(&g1);
 	printf("Thread nr %d done\n", a1);
+	green_join(&g2);
+	printf("Thread nr %d done\n", a2);
+	green_join(&g3);
+	printf("Thread nr %d done\n", a3);
+	printf("count is %d\n", counter);
 	printf("done\n");
 	return 0;
 }
